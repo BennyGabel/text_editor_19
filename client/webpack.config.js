106841ae1989bb197b1a7e818afe93f3a9245d4d
13 Miles <bennygabel@gmail.com>
2022-12-07 16:18:22 -0500

    Missing in the project, 2 issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,14 +17,65 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
 
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new WebpackPwaManifest({
+          name: 'Text Editor - JATE - Application',
+          short_name: 'JATE',
+          description: 'Text editor to track of important information!',
+          background_color: '#7eb4e2',
+          theme_color: '#7eb4e2',
+          start_url: '/',
+          publicPath: '/',
+          icons: [
+            {
+              // src: path.resolve('src/images/icon-manifest.png'),
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            },
+            // {
+            //   // src: path.resolve('src/images/icon-manifest.png'),
+            //   src: path.resolve('src/images/logo.png'),
+            //   size: '1024x1024',
+            //   destination: path.join('assets', 'icons'),
+            //   purpose: 'maskable'
+            // }
+          ],
+        }), 
+        new InjectManifest({
+          // swSrc: './sw.js',
+          swSrc: './src-sw.js',
+          // swDest: 'service-worker.js',
+          swDest: 'src-sw.js'
+        }), 
+      ],
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
-};
+}
